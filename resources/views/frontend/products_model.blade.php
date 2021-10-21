44a5705e36e44f15d75fab996989e5f9e694713f
nihsaan7 <nihsaan.enspirer@gmail.com>
2021-10-21 15:56:49 +0530

    text center

--- resources/views/frontend/products_model.blade.php
+++ resources/views/frontend/products_model.blade.php
@@ -263,8 +263,8 @@
                     <div class="mb-2">
                       <label for="contact-number" class="form-label">Contact Number</label>
                         <div class="row">
-                          <div class="col-2 pe-0 text-center">
-                            <input type="text" class="form-control" value="+94 " disabled>
+                          <div class="col-2 pe-0">
+                            <input type="text" class="form-control text-center" value="+94 " disabled>
                           </div>
                           <div class="col-10 ps-0">
                             <input type="number" class="form-control" name="contact_number" required>
