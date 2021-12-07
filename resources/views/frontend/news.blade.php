cdbd3b514fc4a96c0ceafac47daa978272523279
prasadbanuka <banuka.mawelage@gmail.com>
2021-12-07 18:08:00 +0530

    iphone fix in news page

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -23,7 +23,7 @@
                 <div class="col-12 col-md-5 p-md-5">
                     <h1 data-aos="fade-down-left" data-aos-duration="500" data-aos-delay="300">{{ $featured_news->title }}</h1>
                     <div style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 6; -webkit-box-orient: vertical;">
-                        {!! $featured_news->description !!}
+                        {{strip_tags($featured_news->description)}}
                     </div>
                     <div class="clearfix">
                         <div class="text-center float-md-end mt-4">
